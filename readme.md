e723f47c93d7f2ffc4bfc5e35d35e6355a85265a
Shaked Tayab <shakedtayab@mac-82239s-MacBook-Pro.local>
2019-09-19 14:38:00 +0300

    created and finished concept project

--- readme.md
+++ readme.md
@@ -8,12 +8,12 @@ Please focus on good coding practices.
 Once done, please send back the new and improved version.
 
 What to do:
-1) Make the piece "hollow" when the mouse hovers over it (hollow means only the border should be drawn with the color of the piece)
-2) The `init` function has too much code duplication - fix it 
-3) Add a 'Reset' button that returns the piece back to its initial location
-4) Add a 'Randomize' button that places the piece at a random location
-5) Make sure that the piece never gets off the screen
-6) When the page loads, get today's temperature from an API and set the piece's color according to the following ranges:
+1) Make the piece "hollow" when the mouse hovers over it (hollow means only the border should be drawn with the color of the piece) - done
+2) The `init` function has too much code duplication - fix it  - dpne
+3) Add a 'Reset' button that returns the piece back to its initial location - done
+4) Add a 'Randomize' button that places the piece at a random location -done
+5) Make sure that the piece never gets off the screen - done
+6) When the page loads, get today's temperature from an API and set the piece's color according to the following ranges: - done 
     - < 10 degrees: Blue
     - 11 - 20 degrees: Green
     - 21 - 30 degrees: Yellow
@@ -22,9 +22,16 @@ What to do:
     You can use this URL to retrieve the weather data for Tel-Aviv:  
     http://api.apixu.com/v1/current.json?key=dda6e762ae4f41efb7e173552192204&q=tel%20aviv
 7) Please describe how would you make the motion of the piece smooth instead of jumpy?  
-   Implement your suggestion for extra bonus!
+   Implement your suggestion for extra bonus! - 
+   ** you can add Animation tranisition using css. implemented in the code.
+
 8) How would you improve the code of this project?   
-Meaning, how would you implement the same application differently (if at all) - did you have parts of the code that you didn't like? would you add anything? Use a different architecture?
+Meaning, how would you implement the same application differently (if at all) - did you have parts of the code that you didn't like? would you add anything? Use a different architecture? 
+
+** I will start by moving the project to a react envoirment and splitting to reusabel components.
+** adding tests
+** splitting to mutiply files
+
 Give some suggestions!
 
 Thanks and good luck!
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,7 @@
+// Becouse of the request that  the color of "hallowed" circle will be the same as the current circle color we need a shared variable.
+// Currently to avoid over complicating or moving the code to a class i chose to use a 'global' variable.
+let oldColor = 'blue';
+
 // piece object
 const piece = (function() {
   let el = null;
@@ -19,26 +23,115 @@ function handleClick(ev) {
   piece.moveDelta(parseInt(this.dataset.dx), parseInt(this.dataset.dy));
 }
 
+const handleMouseOver = () => {
+  var circle = document.getElementById('piece'); 
+  oldColor = circle.style.backgroundColor;
+
+  circle.style.border = '1px solid';
+  circle.style.borderColor = oldColor;
+  circle.style.backgroundColor = 'white';
+}
+
+const handleMouseLeave = () => {
+  var circle = document.getElementById('piece'); 
+  circle.style.border = '0px';
+  circle.style.backgroundColor = oldColor; 
+}
+
 function init() {
-  const $btnUp = document.getElementById("btn-up");
-  $btnUp.dataset.dx = 0;
-  $btnUp.dataset.dy = -100;
-  $btnUp.addEventListener("click", handleClick);
-  const $btnRight = document.getElementById("btn-right");
-  $btnRight.dataset.dx = 100;
-  $btnRight.dataset.dy = 0;
-  $btnRight.addEventListener("click", handleClick);
-  const $btnDown = document.getElementById("btn-down");
-  $btnDown.dataset.dx = 0;
-  $btnDown.dataset.dy = 100;
-  $btnDown.addEventListener("click", handleClick);
-  const $btnLeft = document.getElementById("btn-left");
-  $btnLeft.dataset.dx = -100;
-  $btnLeft.dataset.dy = 0;
-  $btnLeft.addEventListener("click", handleClick);
+  handleButtonListenerSetUp();
+  handleResetButtonSetup();
+  handleRandomButtonSetUp();
+  getAndSetCircleColor();
+  handleCircleMouseOverActions();
 }
 
 window.addEventListener("DOMContentLoaded", event => {
   piece.init(document.getElementById("piece"));
   init();
 });
+
+const handleCircleMouseOverActions = () => {
+  var circle = document.getElementById('piece');
+  circle.onmouseover = handleMouseOver;
+  circle.onmouseleave = handleMouseLeave;
+};
+
+const handleResetButtonSetup = () => {
+  const resetButton = document.getElementById('reset');
+
+  resetButton.addEventListener("click", resetCircleLocation);
+};
+
+const createButton = (btn, x, y) => {
+  btn.dataset.dx = x;
+  btn.dataset.dy = y;
+
+  btn.addEventListener("click", handleClick);
+};
+
+const resetCircleLocation = () => {
+  var circle = document.getElementById('piece');
+  
+  // first reset all styles
+  circle.style = '';
+  // then set the color that came back from the api.
+  circle.style.backgroundColor = oldColor;
+};
+
+const handleButtonListenerSetUp = () => {
+  const $btnUp = createButton(document.getElementById("btn-up"), 0, -100);
+  const $btnRight =  createButton(document.getElementById("btn-right"), 100, 0);
+  const $btnDown =  createButton(document.getElementById("btn-down"), 0, 100);
+  const $btnLeft = createButton(document.getElementById("btn-left"), -100, 0); 
+};
+
+const handleRandomButtonSetUp = () => {
+  const randomButton = document.getElementById('random');
+
+  randomButton.addEventListener("click",  randomizeLocation);
+};
+
+const randomizeLocation = () => {
+  // Not all browsers support window.innerWidth/height, i  add also fall back on document.
+  const screenHeight = (window.innerHeight || document.documentElement.clientHeight);
+  const screenWidth = (window.innerWidth || document.documentElement.clientHeight);
+
+  // calculate height and width randomly from 1 to screen height/ screen width.
+  const height = (Math.random() * screenHeight) + 1;
+  const width = (Math.random() * screenWidth) + 1;
+ 
+  /** 
+   * i chose not to use handleClick functionlity becouse the naming can be confusing.
+   * It also goes through a lot  of fucntinality that isn't need in this function.
+   * */
+  const circle = document.getElementById('piece');
+  circle.style.left = `${width}px`;
+  circle.style.top = `${height}px`;
+};
+
+// Get and set circle color  from the api
+const getAndSetCircleColor = () => {
+  fetch('http://api.apixu.com/v1/current.json?key=dda6e762ae4f41efb7e173552192204&q=tel%20avi')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    const temp = myJson.current.temp_c;
+    const circle = document.getElementById('piece');
+    let colorToSet;
+  
+    if(temp <= 10) {
+      colorToSet = 'blue';
+    } else if (temp <= 20) {
+      colorToSet = 'green';
+    } else if (temp <= 30) {
+      colorToSet = 'yellow';
+    } else  {
+      colorToSet = 'red';
+    }
+
+    circle.style.backgroundColor = colorToSet;
+    oldColor = colorToSet;
+  });
+};
